b18c0cb3345c66d98c7b94f3d4bf055cac21c402
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2019-11-22 21:12:28 +0100

    Server main into class fill

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,39 @@
 package client;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import data.Person;
 
 public class Client {
 
+	
+	public static void main(String [] args) {
+		try {
+			Socket socket = new Socket("localhost",8080);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("XD");
+			String line, income;
+			boolean decision= true;
+			while(decision) {
+				if((line = stdIn.readLine()) != null) {
+					if(line.contentEquals("q")) {
+						decision = !decision;
+					}
+					else {
+						System.out.println("Client sent: " + line);
+					}
+					out.println(line);
+				}
+				if((income = in.readLine()) != null) {
+					System.out.println("Client received: " + income);
+				}
+			}
+			socket.close();
+			System.out.println("CLIENT ACTIONS ENDED");
+		}catch (Exception e) {
+			System.err.println(e);
+		}
+	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,14 +1,48 @@
 package server;
 import java.util.*;
 import data.*;
+import java.net.*;
+import java.io.*;
 
 public class Server {
 	PersonTree data;
+	ServerSocket server;
+	PrintWriter output;
+	BufferedReader input;
 	
-	public Server(String path) {
+	public Server(String path) throws Exception{
+		System.out.println("Starting server");
 		this.data = new PersonTree(path);
+		this.server = new ServerSocket(8080);
+		System.out.println("Waiting for connection");
+		Socket cs = this.server.accept();
+		System.out.println("Connection established");
+		this.output = new PrintWriter(cs.getOutputStream(),true);
+		this.input = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+	}
+	public void run() throws Exception{
+		String inl, outl;
+		outl = "Napisz cos ['q' konczy wymianę]";
+		while((inl = this.input.readLine()) != null) {
+			if(inl.contentEquals("q")) break;
+			System.out.println(inl);
+			this.output.println(outl);
+		}
+		System.out.println("out");
+		this.finalize();
 	}
 	
+	@Override
+	public void finalize(){
+		try {
+			System.out.println("Closing server");
+			this.server.close();
+		} catch (Exception e) {
+			System.out.println("Exception during server closure ocurred:");
+			System.err.println(e);
+		}
+		System.out.println("finalizing");
+	}
 //	public Server(Person[] people) {
 //		this.data = new TreeSet<Person>();
 //		for(Person person:people) {
@@ -18,10 +52,13 @@ public class Server {
 	
 	
 	public static void main(String args[]) {
-//		Person[] prs = {new Person("XD","XD","Dd",85123),new Person("D","XD","Dd",13), new Person("XsssD","XsD","Dd",123)};
-//		Server srv2 = new Server(prs);
-//		System.out.println(srv2.data.size());
-//		for(Person pr: srv2.data)
-//			System.out.println(pr);
+		System.out.println("SYSTEM START");
+		try {
+			Server srv = new Server("/home/michal/Programming/TCPServer/src/data/data");
+			srv.run();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		System.out.println("SERVER END");
 	}
 }
