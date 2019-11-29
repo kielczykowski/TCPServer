f09719785187d77ba8512c41194e816e5ba66491
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2019-11-29 22:34:02 +0100

    WIP XD

--- src/client/Client.java
+++ src/client/Client.java
@@ -23,36 +23,24 @@ public class Client {
 		this.output.println(message);
 	}
 	
-	public void run() throws Exception{
-		String line, incoming;
-		String [] splitted = new String[8];
-		boolean decision= true;
-		while(decision) {
-			if((line = this.stdInput.readLine()) != null) {
-				if(line.contentEquals("q")) {
-					decision = !decision;
-				}
-				else {
-					System.out.println("Client sent: " + line);
-				}
-				this.output.println(line);
-			}
-			if((incoming = this.input.readLine()) != null) {
-				if(!incoming.contentEquals("User with this id wasnt found")) {
-					System.out.println(incoming);
-					splitted = incoming.split(":");
-					for(String s:splitted) {
-						System.out.println(s);
-					}
-					received.add(new Person(splitted[7],splitted[1], splitted[3],Integer.parseInt(splitted[5])));
-					System.out.println(this.received);
-				}
-				System.out.println("Client received: " + incoming);
-			}
-		}
-		System.out.println("Closing Client");
-		this.finalize();
-	}
+// 	public void run() throws Exception{
+// 		String incoming;
+// 		String [] splitted = new String[8];
+// 			if((incoming = this.input.readLine()) != null) {
+// 				if(!incoming.contentEquals("User with this id wasnt found")) {
+// 					System.out.println(incoming);
+// 					splitted = incoming.split(":");
+// 					for(String s:splitted) {
+// 						System.out.println(s);
+// 					}
+// 					received.add(new Person(splitted[7],splitted[1], splitted[3],Integer.parseInt(splitted[5])));
+// 					System.out.println(this.received);
+// 				}
+// 				System.out.println("Client received: " + incoming);
+// 		}
+// //		System.out.println("Closing Client");
+// //		this.finalize();
+// 	}
 	
 	public void save(String path) throws Exception{
 		Collections.sort(this.received);
@@ -66,7 +54,7 @@ public class Client {
 	public void finalize() {
 		try {
 			this.socket.close();
-			this.save("output.txt");
+			this.save("output12121.txt");
 		} catch (Exception e) {
 			System.out.println("Error closing client connection");
 			System.err.println(e);
@@ -77,7 +65,7 @@ public class Client {
 	public static void main(String [] args) {
 		try {
 			Client client = new Client("localhost",8080);
-			client.run();
+			// client.run();
 		}catch (Exception e) {
 			System.err.println(e);
 		}
--- src/client/ClientGui.java
+++ src/client/ClientGui.java
@@ -2,107 +2,268 @@ package client;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.Collection;
+import java.util.Collections;
 
 import client.Client;
 import data.Person;
 
 public class ClientGui {
+	// Client client;
+	// JLabel received;
+	// JTextField text;
+	
+	// static boolean ended = false;
+	
+	// public void initializeGui() {
+	// 	JFrame frame = new JFrame("Gui Client");
+	// 	frame.setLayout(new GridLayout(3,3));
+		
+	// 	JLabel title = new JLabel("CLIENT GUI");
+	// 	frame.add(title);
+		
+	// 	this.received = new JLabel("Here is presented the incoming data from server\n");
+	// 	frame.add(received);
+		
+		
+	// 	this.text = new JTextField();
+	// 	frame.add(text);
+		
+	// 	JButton connect = new JButton("connect");
+	// 	connect.addActionListener(new ActionListener() {
+	// 		public void actionPerformed(ActionEvent e) {
+	// 			try {
+	// 				System.out.println("Starting client");
+	// 				client= new Client("localhost",8080);
+	// 			}catch (Exception ex) {
+	// 				System.err.println(ex);
+	// 			}
+	// 		}
+	// 	});
+	// 	frame.add(connect);
+		
+	// 	JButton showList = new JButton("Show received");
+	// 	showList.addActionListener(new ActionListener() {
+	// 		public void actionPerformed(ActionEvent e) {
+	// 			StringStream ss = new StringStream();
+				
+	// 			received.setText(client.received);
+	// 		}
+	// 	});
+		
+	// 	JButton send = new JButton ("Send");
+	// 	send.addActionListener(new ActionListener() {
+	// 		public void actionPerformed(ActionEvent e) {
+	// 			if(text.getText() == "q") {
+	// 				ended = true;
+	// 			}
+	// 			client.sendMessage(text.getText());
+	// 		}
+	// 	});
+	// 	frame.add(send);
+		
+	// 	JButton exit = new JButton("EXIT");
+	// 	frame.add(exit);
+		
+	// 	frame.setSize(300,300);
+	// 	frame.setVisible(true);
+	// 	frame.pack();
+	// }
+	
+	// public ClientGui() {
+	// 	this.initializeGui();
+		
+	// }
+	
+	// public Client getClient() {
+	// 	if(this.client == null) {
+	// 		try {
+	// 		this.client = new Client("localhost",8080);
+	// 		} catch (Exception e) {
+	// 			System.err.println(e);
+	// 		}
+	// 		return this.client;
+	// 	} else
+	// 	{
+	// 		return this.client;
+	// 	}
+	// }
+
+	JFrame frame;
+	JLabel title;
+	JButton connect;
+	JButton send;
+	JTextArea output;
+	JTextField input;
+	boolean exit;
+
 	Client client;
-	static boolean ended = false;
-	
-	public void initializeGui() {
-		JFrame frame = new JFrame("Gui Client");
-		frame.setLayout(new GridLayout(3,3));
-		
-		JLabel title = new JLabel("CLIENT GUI");
-		frame.add(title);
-		
-		JLabel received = new JLabel("Here is presented the incoming data from server\n");
-		frame.add(received);
-		
-		
-		JTextField text = new JTextField();
-		frame.add(text);
-		
-		JButton connect = new JButton("connect");
-		connect.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					System.out.println("Starting client");
-					client= new Client("localhost",8080);
-				}catch (Exception ex) {
-					System.err.println(ex);
-				}
-			}
-		});
-		frame.add(connect);
-		
-		JButton send = new JButton ("Send");
-		send.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(text.getText() == "q") {
-					ended = true;
-				}
-				client.sendMessage(text.getText());
-			}
-		});
-		frame.add(send);
-		
-		JButton exit = new JButton("EXIT");
-		frame.add(exit);
-		
-		frame.setSize(300,300);
-		frame.setVisible(true);
-		frame.pack();
-	}
-	
+
 	public ClientGui() {
-		this.initializeGui();
-		
+		this.exit = false;
 	}
-	
-	public Client getClient() {
-		if(this.client == null) {
-			try {
-			this.client = new Client("localhost",8080);
-			} catch (Exception e) {
-				System.err.println(e);
+
+
+	public void fillOutput(){
+		StringBuilder str = new StringBuilder();
+		Collections.sort(this.client.received);
+		str.append("Here is incoming output presented\n");
+		try{
+			System.out.println("XDXD");
+			for(Person pr: client.received){
+				str.append(pr + "\n");
 			}
-			return this.client;
-		} else
-		{
-			return this.client;
+		} catch (NullPointerException nl) {
+			JOptionPane.showMessageDialog(null, "Firstly connect to server!");
 		}
+		output.setText(str.toString());
+	}
+
+
+
+
+	public void initGui(){
+		this.frame = new JFrame();
+
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				if (JOptionPane.showConfirmDialog(frame, 
+					"Are you sure you want to close this window?", "Close Window?", 
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+					exit = true;
+					try{
+						client.finalize();
+					} catch(Exception e) {
+						System.err.println(e);
+					}
+					System.exit(0);
+				}
+			}
+		});
+
+		this.title = new JLabel("Client gui");
+		
+		frame.add(title,BorderLayout.NORTH);
+
+		this.connect = new JButton("Connect");
+		connect.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				System.out.println("connect");
+				try {
+					client = new Client("localhost",8080);
+					} catch (Exception e) {
+					System.err.println(e);
+					}
+			}
+		});	
+		frame.add(connect,BorderLayout.LINE_START);
+
+		this.send = new JButton("Send message");
+		send.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				try{
+				if(client.socket.isConnected()){				
+				client.output.println(input.getText());
+				System.out.println("Message sent");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Firstly connect to server!");
+				}
+			} catch(NullPointerException nl) {
+				JOptionPane.showMessageDialog(null, "Firstly connect to server!");
+			}
+			}
+		});
+
+		frame.add(send, BorderLayout.LINE_END);
+
+		this.output = new JTextArea("Here is incoming output presented\n");
+		output.setAutoscrolls(true);
+		output.setEnabled(false);
+		frame.add(output,BorderLayout.CENTER);
+
+		this.input = new JTextField();
+		frame.add(input,BorderLayout.SOUTH);
+
+		frame.setSize(600,500);
+		frame.setVisible(true);
+
 	}
 	
 	public static void main (String args[]) {
-		ClientGui cl = new ClientGui();
-		String incoming;
-		String [] splitted = new String[8];
-//		while(!ClientGui.ended) {
-//			try {
-//				if((incoming = cl.client.input.readLine()) != null) {
-//					System.out.println("WSzed");
-//					if(!incoming.contentEquals("User with this id wasnt found")) {
-//						System.out.println(incoming);
-//						splitted = incoming.split(":");
-//						for(String s:splitted) {
-//							System.out.println(s);
-//						}
-//						cl.client.received.add(new Person(splitted[7],splitted[1], splitted[3],Integer.parseInt(splitted[5])));
-//						System.out.println(cl.client.received);
-//					}
-//					System.out.println("Client received: " + incoming);
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
+		ClientGui xd = new ClientGui();
+		xd.initGui();
+		while(!xd.exit){
+			System.out.println("WSZED");
+			String incoming;
+			String [] splitted = new String[8];
+			if(xd.client != null){
+					try{
+					if((incoming = xd.client.input.readLine()) != null) {
+						if(!incoming.contentEquals("User with this id wasnt found")) {
+							System.out.println(incoming);
+								splitted = incoming.split(":");
+								for(String s:splitted) {
+									System.out.println(s);
+								}
+								xd.client.received.add(new Person(splitted[7],splitted[1], splitted[3],Integer.parseInt(splitted[5])));
+								// System.out.println(this.received);
+						}
+						System.out.println("Client received: " + incoming);
+					}
+					// System.out.println("Closing Client");				
+					System.out.println(xd.client == null);
+					
+					System.out.println("is not null");
+					System.out.println(xd.client.received);
+					xd.fillOutput();
+				} catch (Exception e) {
+					System.out.println("XDDX");
+					System.err.println(e);
+				}
+			}
+		}
+		
+// 		ClientGui cl = new ClientGui();
+// 		while(!ClientGui.ended) {
+// 			try {
+// 			cl.client.run();
+// 			} catch (Exception e) {
+// 				System.err.println(e);
+// 			}
+// 			if(cl.client.received.size() == 0) {
+// 				cl.received.setText("Here incoming will be displayed\n");
+// 			} else {
+// 				cl.received.setText(cl.text.getText());
+// 			}
+// 		}
+// 		cl.client.finalize();
+// //		while(!ClientGui.ended) {
+// //			try {
+// //				if((incoming = cl.client.input.readLine()) != null) {
+// //					System.out.println("WSzed");
+// //					if(!incoming.contentEquals("User with this id wasnt found")) {
+// //						System.out.println(incoming);
+// //						splitted = incoming.split(":");
+// //						for(String s:splitted) {
+// //							System.out.println(s);
+// //						}
+// //						cl.client.received.add(new Person(splitted[7],splitted[1], splitted[3],Integer.parseInt(splitted[5])));
+// //						System.out.println(cl.client.received);
+// //					}
+// //					System.out.println("Client received: " + incoming);
+// //				}
+// //			} catch (Exception e) {
+// //				e.printStackTrace();
+// //			}
+// //		}
 		
 //		while (true) {
 //			System.out.println("XD");
 //		}
 		
-	}
 
+}
 }
